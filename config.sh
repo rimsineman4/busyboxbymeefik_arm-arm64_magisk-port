82b4d3dcbafa652031ca8bd62648ba1b481ac317
Anton Islitzer <a.islitzer2365@gmail.com>
2017-11-18 22:07:28 +0100

    Change permission dir to /bin instead of /xbin...
    ...because I moved the binary to /bin. 
    Also last check before first release..!

--- config.sh
+++ config.sh
@@ -90,5 +90,5 @@ set_permissions() {
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
 
-  set_perm  $MODPATH/system/xbin/busybox  0  2000  0755  u:object_r:busybox_exec:s0
+  set_perm  $MODPATH/system/bin/busybox  0  2000  0755  u:object_r:busybox_exec:s0
 }
